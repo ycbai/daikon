de21e1e02486473e3596b35e74dba0032ceebd3a
Sebastien Gandon <sgandon@talend.com>
2015-09-15 14:13:52 +0200

    rename TalendExceptionContext into ExceptionContext, updated tests

--- daikon/src/main/java/org/talend/daikon/exception/TalendRuntimeException.java
+++ daikon/src/main/java/org/talend/daikon/exception/TalendRuntimeException.java
@@ -19,19 +19,19 @@ import com.fasterxml.jackson.core.JsonGenerator;
  */
 public class TalendRuntimeException extends RuntimeException {
 
-    private static final long            serialVersionUID = -5306654994356243153L;
+    private static final long serialVersionUID = -5306654994356243153L;
 
     /** This class' logger. */
-    private static final Logger          LOGGER           = LoggerFactory.getLogger(TalendRuntimeException.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(TalendRuntimeException.class);
 
     /** The error code for this exception. */
-    private final ErrorCode              code;
+    private final ErrorCode code;
 
     /** The exception cause. */
-    private final Throwable              cause;
+    private final Throwable cause;
 
     /** Context of the error when it occurred (used to detail the user error message in frontend). */
-    private final TalendExceptionContext context;
+    private final ExceptionContext context;
 
     /**
      * Full constructor.
@@ -40,17 +40,13 @@ public class TalendRuntimeException extends RuntimeException {
      * @param cause the root cause of this error.
      * @param context the context of the error when it occurred (used to detail the user error message in frontend).
      */
-    public TalendRuntimeException(ErrorCode code, Throwable cause, TalendExceptionContext context) {
-        super(code.getCode(), cause);
+    public TalendRuntimeException(ErrorCode code, Throwable cause, ExceptionContext context) {
+        super(code.getCode() + ":" + context != null ? context.toString() : "", cause); //$NON-NLS-1$ //$NON-NLS-2$
         this.code = code;
         this.cause = cause;
-        if (context == null) {
-            this.context = TalendExceptionContext.build();
-        } else {
-            this.context = context;
-        }
-
+        this.context = (context == null ? ExceptionContext.build() : context);
         checkContext();
+
     }
 
     /**
@@ -69,7 +65,7 @@ public class TalendRuntimeException extends RuntimeException {
      * @param code the error code that holds all the .
      * @param context the exception context.
      */
-    public TalendRuntimeException(ErrorCode code, TalendExceptionContext context) {
+    public TalendRuntimeException(ErrorCode code, ExceptionContext context) {
         this(code, null, context);
     }
 
@@ -79,7 +75,7 @@ public class TalendRuntimeException extends RuntimeException {
      * @param code an error code serialized to JSON.
      */
     public TalendRuntimeException(JsonErrorCode code) {
-        this(code, TalendExceptionContext.build().from(code.getContext()));
+        this(code, ExceptionContext.build().from(code.getContext()));
     }
 
     /**
