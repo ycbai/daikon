bf08f811dc49950ccf304e607dedfc1720b459fe
Francis Upton IV <fupton@talend.com>
2016-04-09 16:20:40 -0700

    TCOMP-143 Support obtaining properties from upstream components

--- daikon/src/main/java/org/talend/daikon/properties/DesignerService.java
+++ daikon/src/main/java/org/talend/daikon/properties/DesignerService.java
@@ -1,7 +1,9 @@
 package org.talend.daikon.properties;
 
 /**
+ * Represents the design-time service which may extend this interface.
  *
+ * There is no implementation of this service at this level.
  */
 public interface DesignerService {
 }
