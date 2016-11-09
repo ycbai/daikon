7765686f5d7fd846e524dbec0ab9c96e5cb12ac6
Ivan Gonchar <gonchar-ivan@users.noreply.github.com>
2016-11-09 11:43:49 +0200

    Bugfix/6.3/tcomp 333 enforcer improvement (#81)
    * refactor(TCOMP-333): Rename fields, methods, add javadocs to make DiOutgoingSchemaEnforcer class more clear
    
    * refactor(TCOMP-333): cache schema related things as instance fields
    
    * refactor(TCOMP-333): implement index map, which maps index arguments to runtime IndexedRecord indexes
    
    * refactor(TCOMP-333): remove implements DiSchemaConstants
    
    * refactor(TCOMP-333): add IndexMappers and Junit-tests for them
    
    * refactor(TCOMP-333): implement DiOutgoingDynamicSchemaEnforcer
    
    * refactor(TCOMP-333): add missing license headers
    
    * refactor(TCOMP-333): refactor DiOutgoingSchemaEnforcer
    
    * refactor(TCOMP-333): implement EnforcerCreator and tests for it
    
    * refactor(TCOMP-333): remove redundant Exception throwings
    
    * refactor(TCOMP-333): add comment about transform dynamic, simplify computeIndexMap method
    
    * style(TCOMP-333): fix formatting for di classes
    
    * refactor(TCOMP-333): remove runtime schema from constructors; retrive runtime schema from the first incoming record as it was before

---
+++ daikon/src/test/java/org/talend/daikon/di/DynamicIndexMapperByNameTest.java
@@ -0,0 +1,137 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2016 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.daikon.di;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertArrayEquals;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.avro.Schema;
+import org.apache.avro.SchemaBuilder;
+import org.hamcrest.collection.IsIterableContainingInOrder;
+import org.junit.Test;
+import org.talend.daikon.avro.SchemaConstants;
+
+/**
+ * Unit-tests for {@link DynamicIndexMapperByName} class
+ */
+public class DynamicIndexMapperByNameTest {
+
+    /**
+     * Checks {@link DynamicIndexMapperByName#computeIndexMap()} returns int array, which size equals n+1 and
+     * with values which equal indexes of corresponding fields in runtime schema, where n - number of fields in design schema
+     * and dynamic field position is at the start in case of schemas contain fields in the same order
+     */
+    @Test
+    public void testComputeIndexMapStartSameOrder() {
+        int[] expectedIndexMap = { -1, 2, 3, 4 };
+
+        Schema designSchema = SchemaBuilder.builder().record("Record") //
+                .prop(DiSchemaConstants.TALEND6_DYNAMIC_COLUMN_POSITION, "0") //
+                .prop(SchemaConstants.INCLUDE_ALL_FIELDS, "true").fields() //
+                .name("col1").type().intType().noDefault() //
+                .name("col2").type().stringType().noDefault() //
+                .name("col3").type().intType().noDefault() //
+                .endRecord(); //
+
+        Schema runtimeSchema = SchemaBuilder.builder().record("Record").fields() //
+                .name("col0_1").type().intType().noDefault() //
+                .name("col0_2").type().intType().noDefault() //
+                .name("col1").type().intType().noDefault() //
+                .name("col2").type().stringType().noDefault() //
+                .name("col3").type().intType().noDefault() //
+                .endRecord(); //
+
+        DynamicIndexMapperByName indexMapper = new DynamicIndexMapperByName(designSchema);
+        int[] actualIndexMap = indexMapper.computeIndexMap(runtimeSchema);
+        assertArrayEquals(expectedIndexMap, actualIndexMap);
+    }
+
+    /**
+     * Checks {@link DynamicIndexMapperByName#computeIndexMap()} returns int array, which size equals n+1 and
+     * with values which equal indexes of corresponding fields in runtime schema, where n - number of fields in design schema
+     * and dynamic field position is at the start in case of schemas contain fields in the different order
+     */
+    @Test
+    public void testComputeIndexMapStartDiffOrder() {
+        int[] expectedIndexMap = { -1, 4, 2, 0 };
+
+        Schema designSchema = SchemaBuilder.builder().record("Record") //
+                .prop(DiSchemaConstants.TALEND6_DYNAMIC_COLUMN_POSITION, "0") //
+                .prop(SchemaConstants.INCLUDE_ALL_FIELDS, "true").fields() //
+                .name("col1").type().intType().noDefault() //
+                .name("col2").type().stringType().noDefault() //
+                .name("col3").type().intType().noDefault() //
+                .endRecord(); //
+
+        Schema runtimeSchema = SchemaBuilder.builder().record("Record").fields() //
+                .name("col3").type().intType().noDefault() //
+                .name("col0_1").type().intType().noDefault() //
+                .name("col2").type().stringType().noDefault() //
+                .name("col0_2").type().intType().noDefault() //
+                .name("col1").type().intType().noDefault() //
+                .endRecord(); //
+
+        DynamicIndexMapperByName indexMapper = new DynamicIndexMapperByName(designSchema);
+        int[] actualIndexMap = indexMapper.computeIndexMap(runtimeSchema);
+        assertArrayEquals(expectedIndexMap, actualIndexMap);
+    }
+
+    /**
+     * Checks {@link DynamicIndexMapperByName#DynamicIndexMapperByName(Schema, Schema)} throws {@link IllegalArgumentException}
+     * if design schema argument doesn't contain dynamic field properties
+     */
+    @Test(expected = IllegalArgumentException.class)
+    @SuppressWarnings("unused")
+    public void testIndexMapperConstructorThrowsException() {
+        Schema designSchema = SchemaBuilder.builder().record("Record").fields() //
+                .name("col0").type().intType().noDefault() //
+                .name("col1").type().stringType().noDefault() //
+                .name("col2").type().intType().noDefault() //
+                .endRecord(); //
+
+        DynamicIndexMapperByName indexMapper = new DynamicIndexMapperByName(designSchema);
+    }
+
+    /**
+     * Checks {@link DynamicIndexMapperByName#computeDynamicFieldsIndexes()} returns list, which contains indexes of runtime
+     * dynamic fields
+     * (i.e. fields, which are present in runtime schema, but are not present in design schema)
+     */
+    @Test
+    public void testComputeDynamicFieldsIndexesDiffOrder() {
+        List<Integer> expectedIndexes = Arrays.asList(1, 3);
+
+        Schema designSchema = SchemaBuilder.builder().record("Record") //
+                .prop(DiSchemaConstants.TALEND6_DYNAMIC_COLUMN_POSITION, "3") //
+                .prop(SchemaConstants.INCLUDE_ALL_FIELDS, "true").fields() //
+                .name("col0").type().intType().noDefault() //
+                .name("col1").type().stringType().noDefault() //
+                .name("col2").type().intType().noDefault() //
+                .endRecord(); //
+
+        Schema runtimeSchema = SchemaBuilder.builder().record("Record").fields() //
+                .name("col0").type().intType().noDefault() //
+                .name("col3_1").type().intType().noDefault() //
+                .name("col2").type().stringType().noDefault() //
+                .name("col3_2").type().intType().noDefault() //
+                .name("col1").type().intType().noDefault() //
+                .endRecord(); //
+
+        DynamicIndexMapperByName indexMapper = new DynamicIndexMapperByName(designSchema);
+        List<Integer> actualIndexes = indexMapper.computeDynamicFieldsIndexes(runtimeSchema);
+        assertThat(actualIndexes, IsIterableContainingInOrder.contains(expectedIndexes.toArray()));
+    }
+}
