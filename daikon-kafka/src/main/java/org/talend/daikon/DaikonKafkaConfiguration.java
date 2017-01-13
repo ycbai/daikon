27f1d75a8902a3ea94193c8d89e3e1041fab460d
Aurélien Fourmi <afourmi@talend.com>
2017-01-13 09:57:58 +0100

    refactor(tds-1210): remove unused import

--- daikon-kafka/src/main/java/org/talend/daikon/DaikonKafkaConfiguration.java
+++ daikon-kafka/src/main/java/org/talend/daikon/DaikonKafkaConfiguration.java
@@ -1,7 +1,6 @@
 package org.talend.daikon;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
